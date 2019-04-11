f23c4bb02bc6f1a8328890878b2a3265b10a608f
wangzhihaisf <1599315665@qq.com>
2019-04-11 12:33:13 +0800

    bug fixed

--- images360/spiders/images.py
+++ images360/spiders/images.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
+from scrapy import Spider, Request
 from urllib.parse import urlencode
+# import json
+#
+# from images360.items import ImageItem
 
-import scrapy
-from scrapy import Request
 
-
-class ImagesSpider(scrapy.Spider):
+class ImagesSpider(Spider):
     name = 'images'
     allowed_domains = ['images.so.com']
     start_urls = ['http://images.so.com/']
 
     def start_requests(self):
-        params = {"ch": "photography", "listtype": "new"}
-        base_url = "http://image.so.com/zj?"
-        for page in range(1, self.settings.get("MAX_PAGE") + 1):
-            params["sn"] = page * 30
-            params = urlencode(params)
+        data = {'ch': 'photography', 'listtype': 'new'}
+        base_url = 'https://image.so.com/zj?'
+        for page in range(1, self.settings.get('MAX_PAGE') + 1):
+            data['sn'] = page * 30
+            params = urlencode(data)
             url = base_url + params
             yield Request(url, self.parse)
 
